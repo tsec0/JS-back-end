6a144a8cb9e09dd650d938d0127643821a7cff6b
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-05-24 15:49:29 +0300

    Added cube manager with details view for cubicle_own

--- cubicle-own/src/controllers/cubeController.js
+++ cubicle-own/src/controllers/cubeController.js
@@ -4,27 +4,27 @@ const cubeManager = require("../managers/cubeManager");
 
 // URL -> Path /cubes/create
 router.get("/create", (req, res) => {
-    console.log(cubeManager.getAll());
-    res.render("create");
+  console.log(cubeManager.getAll());
+  res.render("create");
 });
 
 router.post("/create", (req, res) => {
-  const { 
-    name, 
-    description, 
-    imageUrl, 
-    difficultyLevel,
-    } = req.body;
+  const { name, description, imageUrl, difficultyLevel } = req.body;
 
   // res.send('Form submitted');
   cubeManager.create({
-    name, 
-    description, 
-    imageUrl, 
+    name,
+    description,
+    imageUrl,
     difficultyLevel: Number(difficultyLevel),
   });
 
   res.redirect("/");
+});
+
+router.get("/:cubeId/details", (req, res) => {
+  const cube = cubeManager.getOne(req.params.cubeId);
+  res.render("details", { ...cube });
 });
 
 module.exports = router;
--- cubicle-own/src/managers/cubeManager.js
+++ cubicle-own/src/managers/cubeManager.js
@@ -1,9 +1,27 @@
 const uniqid = require('uniqid');
 // const db = require('../db.json');
-const cubes = [];
+const cubes = [
+    {
+        id: '2yvdusli1ncvnq',
+        name: '1',
+        description: '2',
+        imageUrl: '3',
+        difficultyLevel: 3
+    },
+
+    {
+        id: '3yvdusli1ncvnq',
+        name: '1',
+        description: '2',
+        imageUrl: '4',
+        difficultyLevel: 4
+    }
+];
 
 // exports.getAll = () => db.cubes.slice();
 exports.getAll = () => cubes.slice();
+
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
 // cubeData = {name, description, difficultyLevel, imageUrl};
 exports.create = (cubeData) => {
