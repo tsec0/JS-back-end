fa3678056f7868bc1bfb9a99cfecaf7667880707
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-05-26 23:25:03 +0300

    Virtual getter method and real method added to Cat.js!

--- lec4-mongoose/models/Cat.js
+++ lec4-mongoose/models/Cat.js
@@ -11,9 +11,15 @@ const catSchema = new mongoose.Schema({
 });
 
 //Add method
+// catSchema.method('greet', function(){}); -> you can do that as well
 catSchema.methods.greet = function(){ // use expression not arrow functions
     console.log(`Hello I\m cat and my name is ${this.name}.`); // instanse of a model
 };
+
+//crerate virtual property
+catSchema.virtual('info').get(function(){
+    return `My name is ${this.name} and I'm ${this.age} years old!`
+})
 
 // Create Model
 const Cat = mongoose.model('Cat', catSchema); // auto pluralise
