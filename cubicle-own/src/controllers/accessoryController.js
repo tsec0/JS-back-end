c4e43e63bb0f59440451bf353bc0e6a8bbf29907
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-05-30 23:52:55 +0300

    Added POST action for create accessory!

--- cubicle-own/src/controllers/accessoryController.js
+++ cubicle-own/src/controllers/accessoryController.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 
 router.get("/create", (req, res) => {
-    res.render('accessory/create');
-  });
+  res.render('accessory/create');
+});
+
+router.post('/create', (req, res) => {
+  const body = req.body;
+
+  console.log(body);
+  //TO DO: Add accessory to DB
+
+  res.redirect('/');
+});
+
 
 module.exports = router;
