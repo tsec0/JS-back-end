60cfcfec9fa1a2b78a6e8b7aaf4d68a6d80aa208
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-05-30 23:48:33 +0300

    Added create functionality for accessory

--- cubicle-own/src/routes.js
+++ cubicle-own/src/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController'); // homeController
 const cubeController = require('./controllers/cubeController'); // cubeController
+const accessoryController = require('./controllers/accessoryController'); //accessoriesController
 
 //Route controller -> middleware / module routs controller
 router.use(homeController);
 router.use('/cubes', cubeController);
+router.use('/accessories', accessoryController);
 
 // 404 Page
 router.get('*', (req, res) => {
