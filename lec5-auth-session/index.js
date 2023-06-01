9153e7852bf20e53acefa6361aa7308fe43d2537
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-06-01 23:17:22 +0300

    Session

--- lec5-auth-session/index.js
+++ lec5-auth-session/index.js
@@ -4,18 +4,25 @@ const { v4: uuid } = require('uuid');
 
 const app = express();
 
+const session = {};
+
 app.use(cookieParser()); // middleware
 
 app.get('/', (req, res) => {
     // let id -> because we use the id later in the .get() scope and we set unique cookie to the id
     // in that way tyhe browser wont set again another cookie
-    let id = uuid(); 
+    let id = '';
 
     const userId = req.cookies['userId'];
 
     if(userId){
         id = userId;
+        console.log('User secret: ', session[userId].secret);
     } else {
+        id = uuid();
+        session[id] = {
+            secret: 'my secret',
+        }
         res.cookie('userId', id);
     }
 
