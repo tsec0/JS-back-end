0c48267f1f8c543be2a05dbcf38a1974e2e966d1
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-06-08 00:10:43 +0300

    Add cube owner!

--- cubicle-own/src/controllers/cubeController.js
+++ cubicle-own/src/controllers/cubeController.js
@@ -19,6 +19,7 @@ router.post("/create", async (req, res) => {
     description,
     imageUrl,
     difficultyLevel: Number(difficultyLevel),
+    owner: req.user._id,
   });
 
   res.redirect("/");
